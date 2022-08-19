a72f363da68bde29c6f70e3312bed30b23dd050c
Samuel <timeuss.sam@gmail.com>
2022-08-19 12:09:11 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '98%',
+        height: '2500px',
+        backgroundImage: 'url(images/bgHome.png)'
+      }}
+    ></div>
+  )
 }
 
-export default App;
+export default App
